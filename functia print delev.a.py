076948c0df3cac515412621be4cb3ff562498896
andreeadelev <andreeadelev@gmail.com>
2026-09-15 10:17:37 +0300

    Add files via upload

---
+++ functia print delev.a.py
@@ -0,0 +1,11 @@
+
+
+a=5
+b=3
+
+print("suma", a, "+",b, "=",a+b, end="\n")
+print("produsul", a, "*",b, "=",a*b, end="\n")
+print("restul", a, "%",b, "=",a%b, end="\n")
+print("diferenta", a, "-",b, "=",a-b, end="\n")
+print("ridicarea la putere", a, "**",b, "=",a**b, end="\n")
+print("catul", a, "//",b, "=",a//b, end="\n")
