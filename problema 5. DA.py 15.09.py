ee2262823ad5c69319f682b52277112169653c03
andreeadelev <andreeadelev@gmail.com>
2026-09-19 12:44:01 +0300

    Add files via upload

---
+++ problema 5. DA.py 15.09.py
@@ -0,0 +1,7 @@
+#problema 5
+x=bool(input("x(0 sau 1) = "))
+y=bool(input("y(0 sau 1) = "))
+print("Rezultatul operatiei logice AND este:", x and y)
+print("Rezultatul operatiei logice OR este:", x or y)
+print("Rezultatul operatiei logice NOT pentru primul numar este:", not x)
+print("Rezultatul operatiei logice NOT pentru al doilea numar este:", not y) 
