02fd213b229896ec70bb04feb83c56cff560d985
andreeadelev <andreeadelev@gmail.com>
2026-09-19 12:26:28 +0300

    Add conversion calculations for natural number n

---
+++ problema 4 DA.py
@@ -0,0 +1,14 @@
+#problema 4
+#Se da un numar natural n:
+n=7
+print( "In centimetri =", n*100)
+print( "In miligrame= ", n*1000000) 
+
+ani=n
+luni=n*12
+saptamani=n*52
+zile=n*365
+print("In ani =", ani)
+print("In luni =", luni)
+print("In saptamani =", saptamani)
+print("In zile =", zile)
