122463f2b95a47a6d0d3e8b6f068c0e7d6e1b7ba
andreeadelev <andreeadelev@gmail.com>
2026-09-22 10:06:14 +0300

    Add files via upload

---
+++ perimetrul problemei 22.09.py
@@ -0,0 +1,6 @@
+lungime = float(input("L = "))
+latime = float(input("l = "))
+aria = lungime * latime
+perimetru = 2 * (lungime + latime)
+print("Aria dreptunghiului este:", round(aria, 2))
+print("Perimetrul dreptunghiului este:", round(perimetru, 2))
