8983962fe702a9aef8be545e5d2273fafa88c374
andreeadelev <andreeadelev@gmail.com>
2026-09-22 10:17:01 +0300

    Add files via upload

---
+++ valori absolute.py
@@ -0,0 +1,10 @@
+import math
+a = int(input("a = "))
+b= int(input("b = "))
+c = int (input("c = "))
+print("numerele introduse sunt: ", a, b, c)
+print ( "valoarea maxima este: ", max(a, b, c))
+print ( "valoarea minima este: ", min(a, b, c))
+print( "valoarea absoluta dintre a si b este: ", abs(a-b))
+
+
